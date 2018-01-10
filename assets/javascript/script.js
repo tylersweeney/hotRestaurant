53fc4e8c6d27840bee9d98afc32b70bbdcf2dbd9
Tyler <tylersweeney@gmail.com>
2018-01-09 21:16:07 -0800

    add folder

---
+++ assets/javascript/script.js
@@ -0,0 +1,24 @@
+$('#tables').click(function() {
+    window.location = "http://localhost:3000/" + this.id;
+ });
+ $('#home').click(function() {
+    window.location = "http://localhost:3000/";
+ });
+ 
+     $("#submit").on("click", function(event) {
+       event.preventDefault();
+       var tables = {
+         name: $("#name").val(),
+         phone: $("#phone").val(),
+         email: $("#email").val(),
+         id: $("#id").val()
+       };
+       alert("Adding Reservation...");
+       $('#form').trigger("reset");
+ 
+       $.post("/api/tables", tables)
+       .then(function(data) {
+         console.log(data);
+         alert("Adding Reservation...");
+       });
+     });
